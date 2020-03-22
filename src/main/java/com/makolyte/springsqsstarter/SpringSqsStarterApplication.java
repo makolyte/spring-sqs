a0e7c47ea54bf233fea36faba39c6c66f6c7d009
unknown <yasmin.rodriguez@gmail.com>
2020-03-22 18:17:58 -0400

    initial project setup

---
+++ src/main/java/com/makolyte/springsqsstarter/SpringSqsStarterApplication.java
@@ -0,0 +1,13 @@
+package com.makolyte.springsqsstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSqsStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSqsStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/makolyte/springsqsstarter/SpringSqsStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makolyte.springsqsstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSqsStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
